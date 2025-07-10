e270d12ddf1c577f7226b786fed15e7fe5fd1c78
Pels28 <kwesiaddopels@gmail.com>
2025-07-09 22:43:10 -1200

    preoared database for production

--- acquisition_backend/settings.py
+++ acquisition_backend/settings.py
@@ -109,25 +109,25 @@ WSGI_APPLICATION = 'acquisition_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# tmpPostgres = urlparse(getenv("DATABASE_URL"))
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': tmpPostgres.path.replace('/', ''),
-#         'USER': tmpPostgres.username,
-#         'PASSWORD': tmpPostgres.password,
-#         'HOST': tmpPostgres.hostname,
-#         'PORT': getenv('PORT'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+tmpPostgres = urlparse(getenv("DATABASE_URL"))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': getenv('PORT'),
+    }
+}
 
 # DATABASES = {
 #     'default': {
